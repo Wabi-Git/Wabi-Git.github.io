f9552c4e5e6cebe7d92f4b39e19142cca3c603dc
Monique Kuhn <monique.kuhn@gmail.com>
2024-06-01 16:03:34 +1000

    Snake Controller Forward Movement + Reset

--- Games/1. Snake Game/My project/Assets/Code/SnakeController.cs
+++ Games/1. Snake Game/My project/Assets/Code/SnakeController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SnakeController : MonoBehaviour
 {
+    public float MoveSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void RestartGame()
+    {
+        // Get the current active scene and reload it
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        // move forward (multiplication by deltaTime is to account for frame drops)
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+        // Restarts game if Key "R" pushed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
     }
 }
