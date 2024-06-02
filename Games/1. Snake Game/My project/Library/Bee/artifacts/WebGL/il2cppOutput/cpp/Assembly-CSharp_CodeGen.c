ef072b765921361faf38b0db870a204d81b30f1b
Monique Kuhn <monique.kuhn@gmail.com>
2024-06-02 14:23:05 +1000

    First build (webGL)

---
+++ Games/1. Snake Game/My project/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,131 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void FoodSpawner_Start_m0EE53C81DC634167270F1F65A292657EAC42393A (void);
+extern void FoodSpawner_SpawnFood_m4B7B3C4A89BB97881CDC9796B925B1C894CF5D4D (void);
+extern void FoodSpawner__ctor_mB736B59A732B476031C2DE5DB6EF877EB4875DB8 (void);
+extern void SnakeController_get_BodyParts_mDE17D117C9C4AF6658406F6D1339A5FEA7FF423D (void);
+extern void SnakeController_set_BodyParts_m27BE65D0EF5E20BA039480B4E2E74EF3C2ECCD28 (void);
+extern void SnakeController_Start_m69D6C06DB6A3F206154B6F77FDD31EE2A2E90C52 (void);
+extern void SnakeController_RestartGame_m7E55854B10C8DC8C630932B638048720167957E8 (void);
+extern void SnakeController_Update_mF19D3D6FA303121B33FB6AD2F32FC93503C68D8F (void);
+extern void SnakeController_GrowSnake_m988F65DF560FE74786B8FF4957538CA0D6AB2D9D (void);
+extern void SnakeController_ShortenSnake_m3D48C8530EF559AA6556F9E3549BD4182471CDEA (void);
+extern void SnakeController_SinkBodyPart_m1E1151DBD45DA9FD0306E0161BB76966FEC43F4E (void);
+extern void SnakeController_FallOffMap_m5315B65877F57BDDB5A68E9B49077A3FE777E487 (void);
+extern void SnakeController__ctor_mF78EC3C8CAC149348FA8D77C157EA2387A252A7A (void);
+extern void U3CFallOffMapU3Ed__24__ctor_m3CCAE0BBB939676CBC0AB596AAEF9B0C21DA755F (void);
+extern void U3CFallOffMapU3Ed__24_System_IDisposable_Dispose_m10F5A25F6B9DF529ECF28A609D61075B30A74C34 (void);
+extern void U3CFallOffMapU3Ed__24_MoveNext_mEF8A1AA61A365F7DBD7DD1B7873DBF6510B3F587 (void);
+extern void U3CFallOffMapU3Ed__24_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m6666041E1055180CB0D499285EFF69BBA18DCA4C (void);
+extern void U3CFallOffMapU3Ed__24_System_Collections_IEnumerator_Reset_m50958FB726F0A0B3576D08D1C0BFD07F14168F99 (void);
+extern void U3CFallOffMapU3Ed__24_System_Collections_IEnumerator_get_Current_mDD0A75C04A30663801F397EC32C1C2A8E973E6E8 (void);
+extern void U3CSinkBodyPartU3Ed__23__ctor_m5D07D128F7E4445173CDE6F7BFBBA973586CF503 (void);
+extern void U3CSinkBodyPartU3Ed__23_System_IDisposable_Dispose_m1621F569E5BCF4CDAAECE31D8D0C1A8E0139DED6 (void);
+extern void U3CSinkBodyPartU3Ed__23_MoveNext_m2CE84B23046C9469209C8C44B9841E66E058EA4C (void);
+extern void U3CSinkBodyPartU3Ed__23_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m93AEC8FDFC5893EAFFB1A6F1072E7ED711362040 (void);
+extern void U3CSinkBodyPartU3Ed__23_System_Collections_IEnumerator_Reset_m4C49CC3B9AE46B401A240D21FCDFA036FF4A4752 (void);
+extern void U3CSinkBodyPartU3Ed__23_System_Collections_IEnumerator_get_Current_mBEF603F99AC8BDE2586B5729C71674C973CAF845 (void);
+extern void SnakeHead_Start_mC981DDB4F874BEF3950B32A462A454FA7A1AADC3 (void);
+extern void SnakeHead_OnTriggerEnter_m4444D8DC3A57E8BB90ED4EFA947488A52FEF5325 (void);
+extern void SnakeHead__ctor_mB409B0EC21DF0F64AB354FED6BD4A1E46FA2A625 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[30] = 
+{
+	FoodSpawner_Start_m0EE53C81DC634167270F1F65A292657EAC42393A,
+	FoodSpawner_SpawnFood_m4B7B3C4A89BB97881CDC9796B925B1C894CF5D4D,
+	FoodSpawner__ctor_mB736B59A732B476031C2DE5DB6EF877EB4875DB8,
+	SnakeController_get_BodyParts_mDE17D117C9C4AF6658406F6D1339A5FEA7FF423D,
+	SnakeController_set_BodyParts_m27BE65D0EF5E20BA039480B4E2E74EF3C2ECCD28,
+	SnakeController_Start_m69D6C06DB6A3F206154B6F77FDD31EE2A2E90C52,
+	SnakeController_RestartGame_m7E55854B10C8DC8C630932B638048720167957E8,
+	SnakeController_Update_mF19D3D6FA303121B33FB6AD2F32FC93503C68D8F,
+	SnakeController_GrowSnake_m988F65DF560FE74786B8FF4957538CA0D6AB2D9D,
+	SnakeController_ShortenSnake_m3D48C8530EF559AA6556F9E3549BD4182471CDEA,
+	SnakeController_SinkBodyPart_m1E1151DBD45DA9FD0306E0161BB76966FEC43F4E,
+	SnakeController_FallOffMap_m5315B65877F57BDDB5A68E9B49077A3FE777E487,
+	SnakeController__ctor_mF78EC3C8CAC149348FA8D77C157EA2387A252A7A,
+	U3CFallOffMapU3Ed__24__ctor_m3CCAE0BBB939676CBC0AB596AAEF9B0C21DA755F,
+	U3CFallOffMapU3Ed__24_System_IDisposable_Dispose_m10F5A25F6B9DF529ECF28A609D61075B30A74C34,
+	U3CFallOffMapU3Ed__24_MoveNext_mEF8A1AA61A365F7DBD7DD1B7873DBF6510B3F587,
+	U3CFallOffMapU3Ed__24_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m6666041E1055180CB0D499285EFF69BBA18DCA4C,
+	U3CFallOffMapU3Ed__24_System_Collections_IEnumerator_Reset_m50958FB726F0A0B3576D08D1C0BFD07F14168F99,
+	U3CFallOffMapU3Ed__24_System_Collections_IEnumerator_get_Current_mDD0A75C04A30663801F397EC32C1C2A8E973E6E8,
+	U3CSinkBodyPartU3Ed__23__ctor_m5D07D128F7E4445173CDE6F7BFBBA973586CF503,
+	U3CSinkBodyPartU3Ed__23_System_IDisposable_Dispose_m1621F569E5BCF4CDAAECE31D8D0C1A8E0139DED6,
+	U3CSinkBodyPartU3Ed__23_MoveNext_m2CE84B23046C9469209C8C44B9841E66E058EA4C,
+	U3CSinkBodyPartU3Ed__23_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m93AEC8FDFC5893EAFFB1A6F1072E7ED711362040,
+	U3CSinkBodyPartU3Ed__23_System_Collections_IEnumerator_Reset_m4C49CC3B9AE46B401A240D21FCDFA036FF4A4752,
+	U3CSinkBodyPartU3Ed__23_System_Collections_IEnumerator_get_Current_mBEF603F99AC8BDE2586B5729C71674C973CAF845,
+	SnakeHead_Start_mC981DDB4F874BEF3950B32A462A454FA7A1AADC3,
+	SnakeHead_OnTriggerEnter_m4444D8DC3A57E8BB90ED4EFA947488A52FEF5325,
+	SnakeHead__ctor_mB409B0EC21DF0F64AB354FED6BD4A1E46FA2A625,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[30] = 
+{
+	1118,
+	1118,
+	1118,
+	1089,
+	944,
+	1118,
+	1118,
+	1118,
+	1118,
+	935,
+	863,
+	1089,
+	1118,
+	935,
+	1118,
+	1063,
+	1089,
+	1118,
+	1089,
+	935,
+	1118,
+	1063,
+	1089,
+	1118,
+	1089,
+	1118,
+	944,
+	1118,
+	2173,
+	1118,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	30,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
